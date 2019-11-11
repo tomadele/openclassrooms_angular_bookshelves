bd8b789635d4496ccbc408f9b74c53c3cff80dd1
tomadele <tom-adele@hotmail.fr>
2019-11-11 15:40:18 +0100

    Ajout du système d'authentification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bookshelves';
+
+  constructor() {
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+      apiKey: 'AIzaSyCyGBrE5SZg-7lUPLn3o-8rr2phOfoWjHI',
+      authDomain: 'ocr-angular-bookshelves-dd64e.firebaseapp.com',
+      databaseURL: 'https://ocr-angular-bookshelves-dd64e.firebaseio.com',
+      projectId: 'ocr-angular-bookshelves-dd64e',
+      storageBucket: 'ocr-angular-bookshelves-dd64e.appspot.com',
+      messagingSenderId: '46550816932',
+      appId: '1:46550816932:web:94cbf9a2729c77608fee31',
+      measurementId: 'G-LS6NF2SXYZ'
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+  }
 }
--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {Observable} from 'rxjs';
+import {ImportManager} from '@angular/compiler-cli/src/ngtsc/translator';
+import * as firebase from 'firebase';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
+export class AuthGuardService implements CanActivate {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return new Promise((resolve, reject) => {
+      firebase.auth().onAuthStateChanged(
+        (user) => {
+          if(user) {
+            resolve(true);
+          } else {
+            this.router.navigate(['/auth', 'signin']);
+            resolve(false);
+          }
+        }
+      );
+    });
+  }
 }
